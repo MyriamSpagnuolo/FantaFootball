ffd5a821e60c3ddaa9c7743337a5987a1efc9d57
Jacopo De maio <jajodemaio@gmail.com>
2026-08-27 17:23:10 +0200

    Implementazione della feature di creazione del calendario da parte dell'admin della lega

--- src/main/java/org/generation/italy/fantafootball/model/repositories/LeagueRepository.java
+++ src/main/java/org/generation/italy/fantafootball/model/repositories/LeagueRepository.java
@@ -4,4 +4,5 @@ import org.generation.italy.fantafootball.model.entities.League;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LeagueRepository extends JpaRepository<League, Long> {
+    boolean existsByIdAndAdminId(Long id, Long adminId);
 }
